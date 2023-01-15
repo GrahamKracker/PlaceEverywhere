55c0c7f9f666f14f9d36b3559db4dc8f3d4badf0
GrahamKracker <gjguzynski@gmail.com>
2023-01-15 00:29:42 -0500

    add option to have towers not be blocked by range

--- Main.cs
+++ Main.cs
@@ -1,5 +1,9 @@
-﻿using BTD_Mod_Helper;
+﻿using System.Linq;
+using BTD_Mod_Helper;
+using BTD_Mod_Helper.Api.ModOptions;
+using BTD_Mod_Helper.Extensions;
 using HarmonyLib;
+using Il2CppAssets.Scripts.Models;
 using Il2CppAssets.Scripts.Models.Towers;
 using Il2CppAssets.Scripts.Simulation.Track;
 using Il2CppAssets.Scripts.Unity.Bridge;
@@ -22,20 +26,39 @@ public class Main : BloonsTD6Mod
     {
         __result = true;
     }
-    
+
     [HarmonyPatch(typeof(Map), nameof(Map.CanPlace))]
     [HarmonyPostfix]
     public static void Map_CanPlace(ref bool __result)
     {
         __result = true;
     }
-    
+
     [HarmonyPatch(typeof(Map), nameof(Map.IsTowerPlaceableInAreaType))]
     [HarmonyPostfix]
     public static void Map_IsTowerPlaceableInAreaType(ref bool __result)
     {
         __result = true;
     }
-    
-    
+
+    public override void OnNewGameModel(GameModel result)
+    {
+        base.OnNewGameModel(result);
+        if (AllowTowersToSeeThroughWalls)
+            foreach (var tower in result.towers)
+            {
+                tower.ignoreBlockers = true;
+                foreach (var attack in tower.GetAttackModels())
+                {
+                    attack.attackThroughWalls = true;
+                    foreach (var projectile in attack.weapons.Select(x=>x.projectile))
+                    {
+                        projectile.ignoreBlockers = true;
+                        projectile.canCollisionBeBlockedByMapLos = false;
+                    }
+                }
+            }
+    }
+
+    private static readonly ModSettingBool AllowTowersToSeeThroughWalls = true;
 }
