b1a059fbef32a984b3d6a4079950cf614197eafe
GrahamKracker <gjguzynski@gmail.com>
2023-01-15 00:10:20 -0500

    Initial Commit

---
+++ Main.cs
@@ -0,0 +1,41 @@
+﻿using BTD_Mod_Helper;
+using HarmonyLib;
+using Il2CppAssets.Scripts.Models.Towers;
+using Il2CppAssets.Scripts.Simulation.Track;
+using Il2CppAssets.Scripts.Unity.Bridge;
+using Il2CppAssets.Scripts.Unity.Towers;
+using Il2CppAssets.Scripts.Unity.UI_New.InGame;
+using MelonLoader;
+using PlaceEverywhere;
+
+[assembly: MelonInfo(typeof(PlaceEverywhere.Main), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+
+namespace PlaceEverywhere;
+
+[HarmonyPatch]
+public class Main : BloonsTD6Mod
+{
+    [HarmonyPatch(typeof(TowerModel), nameof(TowerModel.IsTowerPlaceableInAreaType))]
+    [HarmonyPostfix]
+    public static void TowerModel_IsTowerPlaceableInAreaType(ref bool __result)
+    {
+        __result = true;
+    }
+    
+    [HarmonyPatch(typeof(Map), nameof(Map.CanPlace))]
+    [HarmonyPostfix]
+    public static void Map_CanPlace(ref bool __result)
+    {
+        __result = true;
+    }
+    
+    [HarmonyPatch(typeof(Map), nameof(Map.IsTowerPlaceableInAreaType))]
+    [HarmonyPostfix]
+    public static void Map_IsTowerPlaceableInAreaType(ref bool __result)
+    {
+        __result = true;
+    }
+    
+    
+}
---
+++ ModHelperData.cs
@@ -0,0 +1,10 @@
+﻿namespace PlaceEverywhere;
+
+public static class ModHelperData
+{
+    public const string Name = "PlaceEverywhere";
+    public const string Description = "PlaceEverywhere by GrahamKracker";
+    public const string Version = "1.0.0";
+    public const string RepoOwner = "GrahamKracker";
+    public const string RepoName = "PlaceEverywhere";
+}
